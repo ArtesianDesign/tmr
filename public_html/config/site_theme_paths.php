5db1e505807a98e0f5dfd8a28d433e68997ea2ca
dpidan <dpidans@gmail.com>
2014-02-14 13:38:57 -0800

    cleanup old cruft

--- public_html/config/site_theme_paths.php
+++ public_html/config/site_theme_paths.php
@@ -1,22 +1,14 @@
-<?php    
-
-defined('C5_EXECUTE') or die(_("Access Denied."));
-
-
+<?php defined('C5_EXECUTE') or die(_("Access Denied."));
 /* 
 	you can override system layouts here  - but we're not going to by default 
 	
 	For example: if you would like to theme your login page with the Green Salad theme,
 	you would uncomment the lines below and change the second argument of setThemeByPath 
 	to be the handle of the the Green Salad theme "greensalad" 
-
 */
 
-/*
 $v = View::getInstance();
-
-$v->setThemeByPath('/login', "yourtheme");
-$v->setThemeByPath('/page_forbidden', "yourtheme");
-$v->setThemeByPath('/register', "yourtheme");
-
-*/
+$v->setThemeByPath('/login', "tmr");
+$v->setThemeByPath('/page_forbidden', "tmr");
+$v->setThemeByPath('/page_not_found', "tmr");
+$v->setThemeByPath('/register', "tmr");
